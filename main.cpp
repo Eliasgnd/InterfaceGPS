1e2b7520a459e44a3e1deda27bb1c2460ab39fe8
Eliasgnd <eliasgnd@gmail.com>
2026-02-17 16:40:15 +0100

    changment pour pi

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <QLoggingCategory>
 #include "mainwindow.h"
 #include "telemetrydata.h"
-#include "gpstelemetrysource.h" // N'oubliez pas cet include
+#include "gpstelemetrysource.h"
 #include <QNetworkProxyFactory>
 #include <QDir>
 #include <QStandardPaths>
@@ -10,10 +10,17 @@
 #include <QCoreApplication>
 
 int main(int argc, char *argv[]) {
+    // 1. FORCE LE MODE X11 (Pour contourner les restrictions Wayland sur le clavier)
+    qputenv("QT_QPA_PLATFORM", QByteArray("xcb"));
 
+    // 2. ACTIVE LE PLUGIN DE CLAVIER AU NIVEAU SYSTÈME
     qputenv("QT_IM_MODULE", QByteArray("qtvirtualkeyboard"));
 
+    // 3. FORCE LE CLAVIER À RESTER DANS LA FENÊTRE (Style embarqué)
     qputenv("QT_VIRTUALKEYBOARD_DESKTOP_DISABLE", QByteArray("1"));
+
+    // Optionnel : Style sombre pour voiture
+    qputenv("QT_VIRTUALKEYBOARD_STYLE", QByteArray("default"));
 
     QLoggingCategory::setFilterRules(
         "qt.network.ssl.warning=true\n"
@@ -31,8 +38,6 @@ int main(int argc, char *argv[]) {
     QNetworkProxyFactory::setUseSystemConfiguration(true);
 
     TelemetryData telemetry;
-
-    // Source GPS réelle
     GpsTelemetrySource gpsSource(&telemetry);
     gpsSource.start("/dev/serial0");
 
